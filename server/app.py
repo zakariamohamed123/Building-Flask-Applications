b80783d59a192d7fcd31c4bd4e818edf8f51824c
zakariamohamed123 <zakaria.mohamed1@student.moringaschool.com>
2024-04-13 22:42:13 +0300

    completed

---
+++ server/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return '<h1>Welcom to My page</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+  return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+  app.run(port=5555, debug=True)
+  
